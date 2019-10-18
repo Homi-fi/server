e1129b3fe91249913a6bd2c7b6695644b0eaf6a9
jackbloo <a.iqbal1404@gmail.com>
2019-10-18 16:49:55 +0700

    double

--- helpers/token.js
+++ helpers/token.js
@@ -4,9 +4,15 @@ const secret = process.env.SECRET_TOKEN
 function generateToken(payload){
     return jwt.sign(payload, secret, { expiresIn: '1h' })
 }
-
-
+/* istanbul ignore next */
+function verifyToken(token){
+    /* istanbul ignore next */
+    return jwt.verify(token, secret)
+    /* istanbul ignore next */
+}
 
 module.exports = {
     generateToken,
+    /* istanbul ignore next */
+    verifyToken
 }
