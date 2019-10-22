014378f1643d8c865e25e8c700525f2de55e3cff
whyt <will.heryanto@gmail.com>
2019-10-22 12:03:24 +0700

    cron testing done

--- controllers/cron.js
+++ controllers/cron.js
@@ -1,33 +1,60 @@
 const { CronTime } = require('cron')
-const QRCode = require('qrcode')
-const { lamp1 } = require('../jobs/scheduler')
+const { lamp1On, lamp1Off, lamp2On, lamp2Off } = require('../jobs/scheduler')
 
 class CronController {
-  static Lamp1(req, res, next) {
+  static Lamp(req, res, next) {
     const { time } = req.params
-    const hour = time.split(':')[0]
-    const minute = time.split(':')[1]
+    const hours = time.split(':')[0]
+    const minutes = time.split(':')[1]
     const status = parseInt(time.split(':')[2])
+    const name = decodeURI(time.split(':')[3])
 
-    if (!!status) {
-      lamp1.stop()
-      lamp1.setTime(new CronTime(`${minute} ${hour} * * *`))
-      lamp1.start()
-      res.status(200).json({ message: 'Cron success' })
-    } else {
-      lamp1.stop()
-      res.status(200).json({ message: 'Cron stopped' })
+    switch (name) {
+      case 'Lamp 1-on':
+        if (!!status) {
+          lamp1On.stop()
+          lamp1On.setTime(new CronTime(`${minutes} ${hours} * * *`))
+          lamp1On.start()
+          res.status(200).json({ message: 'Lamp 1-on cron started' })
+        } else {
+          lamp1On.stop()
+          res.status(200).json({ message: 'Lamp 1-on cron stopped' })
+        }
+        break
+      case 'Lamp 1-off':
+        if (!!status) {
+          lamp1Off.stop()
+          lamp1Off.setTime(new CronTime(`${minutes} ${hours} * * *`))
+          lamp1Off.start()
+          res.status(200).json({ message: 'Lamp 1-off cron started' })
+        } else {
+          lamp1Off.stop()
+          res.status(200).json({ message: 'Lamp 1-off cron stopped' })
+        }
+        break
+      case 'Lamp 2-on':
+        if (!!status) {
+          lamp2On.stop()
+          lamp2On.setTime(new CronTime(`${minutes} ${hours} * * *`))
+          lamp2On.start()
+          res.status(200).json({ message: 'Lamp 2-on cron started' })
+        } else {
+          lamp2On.stop()
+          res.status(200).json({ message: 'Lamp 2-on cron stopped' })
+        }
+        break
+      case 'Lamp 2-off':
+        if (!!status) {
+          lamp2Off.stop()
+          lamp2Off.setTime(new CronTime(`${minutes} ${hours} * * *`))
+          lamp2Off.start()
+          res.status(200).json({ message: 'Lamp 2-off cron started' })
+        } else {
+          lamp2Off.stop()
+          res.status(200).json({ message: 'Lamp 2-off cron stopped' })
+        }
+        break
     }
-  }
-
-  static qr(req, res, next) {
-    // QRCode.toDataURL('I am a pony!', function (err, url) {
-    //   res.status(200).json({ message: url })
-    // })
-
-    QRCode.toString('I am a pony!', { type: 'terminal' }, function (err, url) {
-      console.log(url)
-    })
   }
 }
 
--- jobs/scheduler.js
+++ jobs/scheduler.js
@@ -1,14 +1,42 @@
+/* istanbul ignore file */
+
 const { CronJob } = require('cron')
 const { Lamp } = require('../apis/firebase')
 
-const lamp1 = new CronJob('* * * * * *', async function () {
+const lamp1On = new CronJob('* * * * * *', async function () {
   try {
     await Lamp.doc('zxkQKGrgpnOIPIfIhRg8').update({ status: true })
-    console.log('cron success')
+    console.log('cron on success')
   } catch (err) {
-    console.log(err, 'masuk error coy')
+    console.log(err, 'masuk error cron on')
   }
 }, null, false, 'Asia/Jakarta')
 
+const lamp1Off = new CronJob('* * * * * *', async function () {
+  try {
+    await Lamp.doc('zxkQKGrgpnOIPIfIhRg8').update({ status: false })
+    console.log('cron off success')
+  } catch (err) {
+    console.log(err, 'masuk error cron false')
+  }
+}, null, false, 'Asia/Jakarta')
 
-module.exports = { lamp1 }
+const lamp2On = new CronJob('* * * * * *', async function () {
+  try {
+    await Lamp.doc('oIqgb45ze2fYsVeIiGIW').update({ status: true })
+    console.log('cron on success')
+  } catch (err) {
+    console.log(err, 'masuk error cron on')
+  }
+}, null, false, 'Asia/Jakarta')
+
+const lamp2Off = new CronJob('* * * * * *', async function () {
+  try {
+    await Lamp.doc('oIqgb45ze2fYsVeIiGIW').update({ status: false })
+    console.log('cron off success')
+  } catch (err) {
+    console.log(err, 'masuk error cron false')
+  }
+}, null, false, 'Asia/Jakarta')
+
+module.exports = { lamp1On, lamp1Off, lamp2On, lamp2Off }
